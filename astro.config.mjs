3b36d667aed718ee0a32dc24b7b3dc3a9211bc2f
tomy1375 <tomy_ramos1991@yahoo.com.ar>
2024-11-01 00:08:21 -0300

    agregado de cards e inicio de aplicacion para reproducir audio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import svelte from '@astrojs/svelte';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), svelte(), react()],
   output:'server'
 });
---
+++ src/components/Player.jsx
@@ -0,0 +1,44 @@
+import { useRef, useState } from "react"
+
+const Pause = () => (
+    <svg role="img" aria-hidden="true" height="16" width="16" viewBox="0 0 16 16" ><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
+)
+
+const Play = () => (
+    <svg role="img" aria-hidden="true" height="16" width="16" viewBox="0 0 16 16" ><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
+)
+
+export function Player() {
+    const[isPlaying, setIsPlaying] = useState(false)
+    const[currentSong, setCurrentSong] = useState(null)
+    const audioRef = useRef()
+
+    const handleClick = () => {
+        if(isPlaying){
+            audioRef.current.pause()
+        } else{
+            audioRef.current.src = `/music/1/01.mp3`
+            audioRef.current.play()
+            audioRef.current.volume = 0.1;
+        }
+        setIsPlaying(!isPlaying)
+    }
+    return(
+        <div className="flex flex-row justify-between w-full px-4 z-50">
+            <div>
+                Current song...
+            </div>
+            <div className="grid place-content-center gap-4 flex-1">
+                <div className="flex justify-center">
+                    <button className="bg-white rounded-full p-2" onClick={handleClick}>
+                        {isPlaying ? <Pause/> : <Play/>}
+                    </button>
+                </div>
+            </div>
+            <div className="grid place-content-center">
+                Volumen
+            </div>
+            <audio ref={audioRef}/>
+        </div>
+    )
+}
